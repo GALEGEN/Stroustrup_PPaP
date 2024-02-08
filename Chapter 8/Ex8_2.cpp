5b0cfe0f0910a983dcce201947a1c226e30a4f6f
GALEGEN <148256252+GALEGEN@users.noreply.github.com>
2024-02-08 21:41:45 +0300

    Add Ex8_2.cpp

---
+++ Chapter 8/Ex8_2.cpp
@@ -0,0 +1,27 @@
+﻿// Write a function print() that prints a vector of ints to cout.
+
+#include <iostream>
+#include <vector>
+
+void print(const std::string& rs, const std::vector<int>& rv)
+// prints a vector of ints to cout
+{
+    for (size_t i = 0; i < rv.size(); i++)
+    {
+        std::cout << rv[i];
+        if (i < rv.size() - 1) {
+            std::cout << ", ";
+        }
+        else {
+            std::cout << std::endl;
+        }
+    }
+}
+
+int main()
+{
+    std::vector<int> vector{ 1, 2, 3, 4, 5 };
+    std::string label{ "Test: " };
+
+    print(label, vector);
+}
